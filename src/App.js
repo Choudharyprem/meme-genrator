9841f6ba5ca9dbffb99af46659dae14b552cb532
premchand <choudharypremchand16@gmail.com>
2022-06-25 20:26:46 +0530

    meme

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+import './App.css';
+import Meme from './Meme';
+function App() {
+  return (
+    <Meme/>
+  )
+}
+
+export default App;
---
+++ src/Meme.js
@@ -0,0 +1,14 @@
+import './Meme.css'
+import Header from "./Components/Header";
+import Meme1 from './Components/Meme1';
+
+
+export default function Meme(){
+    return(
+        <>
+        <Header/>
+        <Meme1/>
+        
+        </>
+    )
+}
